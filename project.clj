b6f5a4d29504abb8e0be33b714eae59d039104cd
Jim Blomo <jim.blomo+github@gmail.com>
2011-01-27 22:59:32 -0800

    modify for 1.2 deps

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.clojars.jim/clojure-couchdb  "0.4.6"
   :description "Clojure interface for Apache CouchDB"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [clj-http "0.1.1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
